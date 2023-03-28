10573d0d92d9913870e120cdeb3c83ed13f4107f
s559152 <123050392+s559152@users.noreply.github.com>
2023-03-27 23:37:33 -0500

    new endpoint

--- app.js
+++ app.js
@@ -29,15 +29,6 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-var express = require('express');
-var router = express.Router();
-
-/* GET mydata page. */
-router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Your Full Name' });
-});
-
-module.exports = router;
 
 // error handler
 app.use(function(err, req, res, next) {
